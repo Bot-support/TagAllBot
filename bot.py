ddcf6f4f4d5af1d7706f8ad84f6da3906e4fe14e
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-09-12 10:22:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,11 +20,6 @@ client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
 moment_worker = []
 
-#cancel
-@client.on(events.NewMessage(pattern='^(?i)/cancel'))
-async def cancel(event):
-  global moment_worker
-  moment_worker.remove(event.chat_id)
 
 #start
 @client.on(events.NewMessage(pattern="^/start$"))
